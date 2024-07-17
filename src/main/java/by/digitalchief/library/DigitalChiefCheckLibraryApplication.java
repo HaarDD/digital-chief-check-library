e7c09ee8837482ca79e8a55ab0ab7e1564466078
HaarDD <maxgalitskiy@gmail.com>
2024-07-17 03:34:29 +0300

    Initial commit

---
+++ src/main/java/by/digitalchief/library/DigitalChiefCheckLibraryApplication.java
@@ -0,0 +1,13 @@
+package by.digitalchief.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalChiefCheckLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalChiefCheckLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/digitalchief/library/DigitalChiefCheckLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package by.digitalchief.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalChiefCheckLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
